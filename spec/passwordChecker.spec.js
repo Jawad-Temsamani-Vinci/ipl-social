b1bfc1e17603c5f3a822d628c8fcd72d2fb3aa7d
Temsamani Jawad <jawad.temsamani@student.vinci.be>
2025-01-17 17:11:50 +0100

    Verify: less than 8 characters

--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -5,4 +5,8 @@ describe("Password Validation", function() {
         const password = "mypassword";
         expect(isPasswordValid(password)).toBe(true);
     });
+    it("should be invalid if less than 8 characters long", function() {
+        const password = "short";
+        expect(isPasswordValid(password)).toBe(false);
+    });
 });
